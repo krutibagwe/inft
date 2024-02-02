c173858da5193b5319acd5486ccdab95ea210c3a
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-02-03 02:38:10 +0530

    Create exp3_0_input.py

---
+++ 4_python/exp3_0_input.py
@@ -0,0 +1,28 @@
+>>> name = input("Enter your name: ")
+Enter your name: Kruti
+>>> name
+'Kruti'
+>>> a= input("enter a value: ")
+enter a value: 10
+>>> a
+'10'
+>>> b= input("enter b value: ")
+enter b value: 20
+>>> b
+'20'
+>>> a+b
+'1020'
+>>> a=int(input("enter value of a: "))
+enter value of a: 10
+>>> a
+10
+>>> b= int(input("enter value of b: "))
+enter value of b: 20
+>>> b
+20
+>>> a+b
+30
+>>> c= float(input("enter a number: "))
+enter a number: 23
+>>> c
+23.0
