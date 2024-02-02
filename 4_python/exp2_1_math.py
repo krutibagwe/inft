a88ed6b155496437e2fc0a1b09ae3ec6a788feca
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-02-03 02:36:28 +0530

    Add files via upload

---
+++ 4_python/exp2_1_math.py
@@ -0,0 +1,29 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> math.ceil(1.4)
+2
+>>> math.floor(1.4)
+1
+>>> math.factorial(5)
+120
+>>> math.fmod(50,3)
+2.0
+>>> math.gcd(48,24)
+24
+>>> math.pow(4,3)
+64.0
+>>> math.remainder(14,3)
+-1.0
+>>> math.trunc(-24.368)
+-24
+>>> math.fsum([2,4,6,8])
+20.0
+>>> math.sqrt(12)
+3.4641016151377544
+>>> math.isqrt(12)
+3
+>>> math.sin(math.pi/2)
+1.0
+>>> math.tan(180)
+1.3386902103511544
---
+++ 4_python/exp2_3_operator.py
@@ -0,0 +1,61 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=2
+>>> a+b
+7
+>>> a-b
+3
+>>> a*b
+10
+>>> a/b
+2.5
+>>> a//b
+2
+>>> a%b
+1
+>>> a**b
+25
+>>> x=10
+>>> x+=5
+>>> x
+15
+>>> x&=3
+>>> x
+3
+>>> a==b
+False
+>>> a!=b
+True
+>>> a>b
+True
+>>> a<=b
+False
+>>> a>4 and b<7
+True
+>>> a>10 or b>1
+True
+>>> not(a<10 and b>0)
+False
+z= ["green", "red"]
+print("red" in z)
+True
+print("green" not in z)
+False
+z1= ["green", "blue"]
+z is z1
+False
+z is not z1
+True
+4&7
+4
+3|8
+11
+6^2
+4
+~6
+-7
+5<<2
+20
+5>>2
+1
