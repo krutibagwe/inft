570181e52e3d63497ae24c3f1e033ded9b268432
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-02-06 03:57:38 +0530

    Create exp4_4_tables.py

---
+++ 4_python/exp4_4_tables.py
@@ -0,0 +1,14 @@
+#for and while loops are used to print the table of number given by user. The loop multiplies i with the inputted number and increments by 1 till it reaches 10
+#FOR LOOP
+num = int(input("enter the number: "))
+print("Table of {}:".format(num))
+for i in range(1,11):
+    print(num*i)
+
+#WHILE LOOP
+i=1
+num =  int(input("enter the number: "))
+print("Table of {}:".format(num))
+while<=10:
+    print(num*i)
+    i+=1
