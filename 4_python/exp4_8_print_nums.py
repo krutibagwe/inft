80729bc294b44416b731d9443674017784a8db5d
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-02-06 04:00:39 +0530

    Create exp4_8_print_nums.py

---
+++ 4_python/exp4_8_print_nums.py
@@ -0,0 +1,8 @@
+#loops are used to print all the numbers from 1 to 100, with each number separated by space
+for i in range(1,100):
+    print(i, end=" ")
+
+i=0
+while i<=100:
+    print(i, end=" ")
+    i+=1 
