800502a6ff40416167e266728db3057edefcc175
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-02-11 02:40:31 +0530

    Create exp5_1_stack.py

---
+++ 4_python/exp5_1_stack.py
@@ -0,0 +1,13 @@
+stack =[]
+stack.append("One")
+stack.append("Two")
+stack
+['One', 'Two']
+stack.append("Three")
+stack.append("Four")
+stack
+['One', 'Two', 'Three', 'Four']
+stack.pop()
+'Four'
+stack
+['One', 'Two', 'Three']
