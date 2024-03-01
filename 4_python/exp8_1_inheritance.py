0038c52f80da0e08d19e088c877dfa21a5b3b956
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-03-02 04:21:42 +0530

    Create exp8_1_inheritance.py

---
+++ 4_python/exp8_1_inheritance.py
@@ -0,0 +1,21 @@
+#parent class
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+p = Person("John", "Doe")
+
+#child class inherits from Parent class
+class Student(Person):
+    def __init__ (self, fname, lname, d):
+        Person.__init__ (self, fname, lname)
+        self.div = d
+        print(self.div)
+
+       
+s1 = Student("ABC", "def", "A")
+s1.printname()
