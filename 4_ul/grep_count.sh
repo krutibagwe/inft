3158614cc6d7c827b67f84cd8bf4eaac4f674fe6
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-04-03 01:25:54 +0530

    Create grep_count.sh

---
+++ 4_ul/grep_count.sh
@@ -0,0 +1,6 @@
+echo "Number of characters in file: "
+grep " " file_name.txt|wc -c
+echo "Number of words in file: "
+grep " " file_name.txt|wc -w
+echo "Number of lines in file: "
+grep " " file_name.txt|wc -l
