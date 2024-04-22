04cddf3118206e3a233ea0cfe072772558546965
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-04-22 18:11:36 +0530

    Create 13_leap_year.py

---
+++ 4_python/pracs/13_leap_year.py
@@ -0,0 +1,10 @@
+#Write a program to check whether the input year is a leap year or not.
+
+while True:
+    year = int(input("enter the year: "))
+    
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
