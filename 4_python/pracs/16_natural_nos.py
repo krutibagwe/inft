8e18595db97e9eca8d323f3b51e21aa4fbca1668
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-04-22 18:37:09 +0530

    Create 16_natural_nos.py

---
+++ 4_python/pracs/16_natural_nos.py
@@ -0,0 +1,11 @@
+#Write a program to print all natural numbers between 1 to 100 using looping statements
+
+for i in range(1,101):
+    print(i, end=" ")
+
+print("\n\n")
+
+num=1
+while num<=100:
+    print(num, end=" ")
+    num +=1
