ca3d81153f7cecb00e0aca7b5d055e978d396980
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-04-23 02:34:56 +0530

    Create 22_factorial_forloop.py

---
+++ 4_python/pracs/22_factorial_forloop.py
@@ -0,0 +1,16 @@
+#Write a program to calculate factorial of a given number using a for loop.
+
+while True:
+    num = int(input("emter the number: "))
+
+    if num<0:
+        print("Enter positive number!")
+
+    else:
+        factorial = 1
+
+        for i in range(1, num+1):
+            factorial *= i
+
+        print(f"Factorial of {num}: {factorial}")
+
