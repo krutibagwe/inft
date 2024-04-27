15a2a00addcc65ade0439fc9d0744f77883aa858
Kruti Bagwe <83567614+krutibagwe@users.noreply.github.com>
2024-04-28 03:29:43 +0530

    Update 22_factorial_forloop.py

--- 4_python/pracs/22_factorial_forloop.py
+++ 4_python/pracs/22_factorial_forloop.py
@@ -1,7 +1,7 @@
 #Write a program to calculate factorial of a given number using a for loop.
 
 while True:
-    num = int(input("emter the number: "))
+    num = int(input("Enter the number: "))
 
     if num<0:
         print("Enter positive number!")
